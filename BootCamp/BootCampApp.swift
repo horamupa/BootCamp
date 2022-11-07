f3d79165a79fba44c9675c2ab089237fb7d4328f
horamupa <horamupa@gmail.com>
2022-11-07 15:35:29 +0400

    Initial Commit

---
+++ BootCamp/BootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootCampApp.swift
+//  BootCamp
+//
+//  Created by MM on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct BootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
