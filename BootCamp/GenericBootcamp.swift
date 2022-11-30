30c10c286601a2271c9977e601c1cb52c71e8df9
horamupa <horamupa@gmail.com>
2022-11-30 19:10:11 +0400

    Generic Bootcamp

---
+++ BootCamp/GenericBootcamp.swift
@@ -0,0 +1,66 @@
+//
+//  GenericBootcamp.swift
+//  BootCamp
+//
+//  Created by MM on 30.11.2022.
+//
+
+import SwiftUI
+
+struct StringModel {
+    var info: String?
+    
+    func removeInfo() -> StringModel {
+        StringModel(info: nil)
+    }
+}
+
+struct GenericModel<T> {
+    var info: T?
+    func removeInfo() -> GenericModel {
+        GenericModel(info: nil)
+    }
+}
+
+class GenericBootcampViewModel: ObservableObject {
+    
+    @Published var stringModel = StringModel(info: "Perfect String")
+    @Published var data = ["go","go2"]
+    @Published var genData = GenericModel(info: true)
+}
+
+struct GenericView<T:View>: View {
+    
+//    let isMetric: Bool
+    let title: String
+    let info: T
+    var body: some View {
+        
+        VStack {
+            Text(title)
+            info
+        }
+    }
+}
+
+struct GenericBootcamp: View {
+    @StateObject var vm = GenericBootcampViewModel()
+    
+    var body: some View {
+        VStack {
+            Text(vm.genData.info?.description ?? "no data")
+            Text(vm.stringModel.info ?? "no data")
+            
+        }
+        .onTapGesture {
+            vm.stringModel = vm.stringModel.removeInfo()
+            vm.genData = vm.genData.removeInfo()
+        }
+    }
+}
+
+struct GenericBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GenericBootcamp()
+    }
+}
